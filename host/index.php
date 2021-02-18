3750aebde48291f1a21d74eeaa1e8adc70e1f67e
= <root@gabhost.tk>
2021-02-18 19:54:23 +0100

    recommented debug index.php also previous commit added |unique filter to twig (unique array)

--- host/index.php
+++ host/index.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+//error_reporting(E_ALL);
+//ini_set("display_errors", 1);
 //charge twig
 session_start();
 if(!empty($_SESSION['pseudo'])){
